f9b1247b2a901ee44d6120a3baefd0ea86b2f816
Ervin <ervingorospe.code@gmail.com>
2026-01-27 12:18:51 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import ThemeChanger from "./ThemeChanger";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl">testing</h1>
+      <ThemeChanger />
+    </div>
+  );
+}
