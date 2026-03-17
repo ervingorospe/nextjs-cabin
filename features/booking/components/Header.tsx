db4ca5f82910837231a80cfe86b89f2222f180ec
Ervin <ervin.c.gorospe@accenture.com>
2026-03-17 19:12:36 +0800

    booking header

--- features/booking/components/Header.tsx
+++ features/booking/components/Header.tsx
@@ -1,10 +1,26 @@
 "use client";
 import PageHeader from "@/components/ui/PageHeader";
+import Plus from "@/components/ui/icons/Plus";
+import IconLayout from "@/components/ui/icons/IconLayout";
+import Form from "@/features/room/components/Form";
+import { defaultValue } from "@/features/room/fields";
+import { Box } from "@mui/material";
 
 export default function Header() {
   return (
-    <div>
-      <PageHeader title="Booking"></PageHeader>
-    </div>
+    <Box>
+      <PageHeader title="Booking">
+        <PageHeader.Modal
+          buttonRender={<span>Book Room</span>}
+          startIcon={
+            <IconLayout className="h-7 w-7">
+              <Plus />
+            </IconLayout>
+          }
+          ModalRender={<Form data={defaultValue} />}
+          ModalTitle="New Room"
+        />
+      </PageHeader>
+    </Box>
   );
 }
